8202c4d1cf7c0f3a79b8e9c9e47cf832ae8f4235
Abdul Ali <abdulqayyum354@gmail.com>
2022-03-18 16:36:15 +0500

    commit

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const collection = document.querySelectorAll('.collection');
 const gardener = document.querySelectorAll('.gardener');
 const roadmap = document.querySelectorAll('.roadmap');
-const harvest = document.querySelectorAll('.harvest'); 
+const harvest = document.querySelectorAll('.harvest');
 const sideNavBar = document.querySelector('.side-nav-bar');
 const sideNavBarMobile = document.querySelector('.side-nav-bar-mobile');
 const wrapper = document.querySelectorAll('.wrapper');
@@ -14,22 +14,22 @@ const hamburgerMenu = document.querySelector('.hamburger-menu');
 const closeBtn = document.querySelector('#close');
 
 
-closeBtn.addEventListener('click', () =>{
+closeBtn.addEventListener('click', () => {
     sideNavBarMobile.classList.remove('left');
 
 })
 
-hamburgerMenu.addEventListener('click', () =>{
+hamburgerMenu.addEventListener('click', () => {
     sideNavBarMobile.classList.toggle('left');
 })
 
 
-home.forEach(home =>{
-    home.addEventListener('click', () =>{
-        wrapper.forEach(wrapper =>{
+home.forEach(home => {
+    home.addEventListener('click', () => {
+        wrapper.forEach(wrapper => {
             wrapper.style.display = 'block';
         })
-    
+
         wrapper2.style.display = 'none';
         wrapper3.style.display = 'none';
         wrapper4.style.display = 'none';
@@ -41,73 +41,61 @@ home.forEach(home =>{
 
 
 
-collection.forEach(collection =>{
-    collection.addEventListener('click', () =>{
+collection.forEach(collection => {
+    collection.addEventListener('click', () => {
         wrapper3.style.display = 'block';
-    wrapper.forEach(wrapper =>{
-        wrapper.style.display = 'none';
-    })
+        wrapper.forEach(wrapper => {
+            wrapper.style.display = 'none';
+        })
 
-    wrapper2.style.display = 'none';
-    wrapper4.style.display = 'none';
-    wrapper5.style.display = 'none';
-    sideNavBar.style.display = 'flex';
+        wrapper2.style.display = 'none';
+        wrapper4.style.display = 'none';
+        wrapper5.style.display = 'none';
+        sideNavBar.style.display = 'flex';
 
     })
 })
 
 
-gardener.forEach(garden =>{
-    garden.addEventListener('click', () =>{
+gardener.forEach(garden => {
+    garden.addEventListener('click', () => {
         wrapper2.style.display = 'block';
-            wrapper.forEach(wrapper =>{
-                wrapper.style.display = 'none';
-            })
-        
-            wrapper3.style.display = 'none';
-            wrapper4.style.display = 'none';
-            wrapper5.style.display = 'none';
-            sideNavBar.style.display = 'flex';
+        wrapper.forEach(wrapper => {
+            wrapper.style.display = 'none';
+        })
+
+        wrapper3.style.display = 'none';
+        wrapper4.style.display = 'none';
+        wrapper5.style.display = 'none';
+        sideNavBar.style.display = 'flex';
     })
 })
 
-roadmap.forEach(roadmap =>{
-    roadmap.addEventListener('click', () =>{
+roadmap.forEach(roadmap => {
+    roadmap.addEventListener('click', () => {
         wrapper4.style.display = 'block';
-    wrapper.forEach(wrapper =>{
-        wrapper.style.display = 'none';
-    })
+        wrapper.forEach(wrapper => {
+            wrapper.style.display = 'none';
+        })
 
-    wrapper2.style.display = 'none';
-    wrapper3.style.display = 'none';
-    wrapper5.style.display = 'none';
-    sideNavBar.style.display = 'none';
+        wrapper2.style.display = 'none';
+        wrapper3.style.display = 'none';
+        wrapper5.style.display = 'none';
+        sideNavBar.style.display = 'none';
     })
 })
 
-harvest.forEach(harvest =>{
-    harvest.addEventListener('click', () =>{
+harvest.forEach(harvest => {
+    harvest.addEventListener('click', () => {
         wrapper5.style.display = 'block';
-    wrapper.forEach(wrapper =>{
-        wrapper.style.display = 'none';
-    })
+        wrapper.forEach(wrapper => {
+            wrapper.style.display = 'none';
+        })
 
-    wrapper2.style.display = 'none';
-    wrapper3.style.display = 'none';
-    wrapper4.style.display = 'none';
-    sideNavBar.style.display = 'flex';
+        wrapper2.style.display = 'none';
+        wrapper3.style.display = 'none';
+        wrapper4.style.display = 'none';
+        sideNavBar.style.display = 'flex';
     })
 })
 
-// harvest.addEventListener('click', () =>{
-//     wrapper5.style.display = 'block';
-//     wrapper.forEach(wrapper =>{
-//         wrapper.style.display = 'none';
-//     })
-
-//     wrapper2.style.display = 'none';
-//     wrapper3.style.display = 'none';
-//     wrapper4.style.display = 'none';
-//     sideNavBar.style.display = 'flex';
-
-// })
